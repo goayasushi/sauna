d46b29ce399ac49d5b1da7facdfa5d62a8a4b0cd
goayasushi <goayasushi@gmail.com>
2023-11-09 21:29:04 +0900

    削除　確認ダイアログ実装

--- member/src/main/resources/static/js/member.js
+++ member/src/main/resources/static/js/member.js
@@ -1,6 +1,15 @@
 const  UPDATE_MESSAGE = "更新してもよろしいですか？"
+const  DELETE_MESSAGE = "削除してもよろしいですか？"
 $('form').submit(function() {
 	if(confirm(UPDATE_MESSAGE)){
+		return true;
+	}else{
+		return false;
+	}
+});
+
+$('#delete').on('click', function() {
+	if(confirm(DELETE_MESSAGE)){
 		return true;
 	}else{
 		return false;
