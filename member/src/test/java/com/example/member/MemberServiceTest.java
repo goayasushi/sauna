109def1e1f26770dd0675f8c1312296f18aaeccb
goayasushi <goayasushi@gmail.com>
2023-12-02 22:35:44 +0900

    JUnitテスト　MemberService

--- member/src/test/java/com/example/member/MemberServiceTest.java
+++ member/src/test/java/com/example/member/MemberServiceTest.java
@@ -1,46 +1,73 @@
 package com.example.member;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.context.ContextConfiguration;
 
 import com.example.member.domain.model.Member;
+import com.example.member.domain.repository.MemberRepository;
 import com.example.member.domain.service.MemberService;
 
 @SpringBootTest
+@ContextConfiguration(classes = TestConfig.class)
 public class MemberServiceTest {
-	MemberService memberService;
+	@Mock
+    private MemberRepository memberRepository;
 
-//    @Autowired
-//    public MemberServiceTest(MemberService memberService) {
-//        this.memberService = memberService;
-//    }
-    
+    private MemberService memberService;
+
     @BeforeEach
     void setUp() {
-        // CustomerMapperのモックを作成
-        memberService = mock(MemberService.class);
-
+        MockitoAnnotations.openMocks(this);
+        memberService = new MemberService(memberRepository);
     }
-    
-	@Test
-	void testFindAll() throws Exception {
-		Member member = new Member();
-		member.setId(1);
-		member.setFirstName("test");
-		member.setLastName("test");
-		Member member2 = new Member();
-		member2.setId(2);
-		member2.setFirstName("test2");
-		member2.setLastName("test2");
-		when(memberService.findAll()).thenReturn(List.of(member, member2));
-		List<Member> result = memberService.findAll();
-		assertEquals(2, result.size());
-	}
+
+    @Test
+    void findAllShouldReturnAllMembers() {
+        List<Member> members = List.of(new Member(), new Member());
+        when(memberRepository.findAll(Sort.by(Sort.Direction.ASC, "id"))).thenReturn(members);
+        List<Member> result = memberService.findAll();
+        assertEquals(2, result.size());
+        verify(memberRepository).findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+
+    @Test
+    void saveShouldPersistMember() {
+        Member member = new Member();
+        when(memberRepository.save(member)).thenReturn(member);
+        Member result = memberService.save(member);
+        assertEquals(member, result);
+        verify(memberRepository).save(member);
+    }
+
+    @Test
+    void findByIdShouldReturnMember() {
+        Member member = new Member();
+        when(memberRepository.findById(1)).thenReturn(Optional.of(member));
+        Member result = memberService.findById(1);
+        assertEquals(member, result);
+        verify(memberRepository).findById(1);
+    }
+
+    @Test
+    void deleteShouldRemoveMember() {
+        Member member = new Member();
+        when(memberRepository.findById(1)).thenReturn(Optional.of(member));
+        doNothing().when(memberRepository).delete(member);
+        memberService.delete(1);
+        verify(memberRepository).findById(1);
+        verify(memberRepository).delete(member);
+    }
 }
---
+++ member/src/test/java/com/example/member/TestConfig.java
@@ -0,0 +1,8 @@
+package com.example.member;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+
+}
