fda66e6445a5fe1486692a44adce0d3653fc18c4
goayasushi <goayasushi@gmail.com>
2023-12-03 20:02:41 +0900

    一覧画面へのパス変更

--- member/src/test/java/com/example/member/MemberControllerTest.java
+++ member/src/test/java/com/example/member/MemberControllerTest.java
@@ -16,7 +16,7 @@ public class MemberControllerTest {
     void testGetMembers(@Autowired MockMvc mvc) throws Exception {
 
         mvc.perform(
-                MockMvcRequestBuilders.get("/member/search"))
+                MockMvcRequestBuilders.get("/members"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
